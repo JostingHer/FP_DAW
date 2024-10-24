3ea8f9150cccc300d05a8026aee7976fb6c29fcc
JostingHer <jostingjmhernandez@gmail.com>
2024-10-24 14:01:52 +0200

    ccc

---
+++ DWServidor/workplace/crudProducts/views/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=h1, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../styles/normalize.css" />
+    <!-- <link rel="stylesheet" href="../styles/style.css" /> -->
+    <link rel="stylesheet" href="../styles/sidebar.css" />
+</head>
+
+<body>
+
+    <!-- <header class="container header">
+        <h2 class="header__title">Mercado Sparking</h2>
+        <div class="header__user">
+            <span><a href="/">Profe user</a> |
+                <a href="/" title="cerrar">Cerrar sesion</a></span>
+        </div>
+
+        <nav class="menu">
+            <a class="menu__link" href="#">Registrar</a>
+
+            <a class="menu__link" href="#">Buscar</a>
+
+            <a class="menu__link" href="#">Total</a>
+        </nav>
+
+    </header> -->
+
+    <?php
+    include("../components/tableProducts.php");
+    ?>
+</body>
+
+</html>
