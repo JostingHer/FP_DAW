c8f0eae4fa9c171e01e0d5217b954a8b6728e0ac
JostingHer <jostingjmhernandez@gmail.com>
2024-11-21 20:22:02 +0100

    cc

--- DWCliente/workplace/tema2/practica2/js/usecases/create-carta-html.js
+++
@@ -1,9 +0,0 @@
-export const crearCartaHTML = (carta) => {
-
-    if(!carta) throw new Error('no has puesto la carta no se cual es la imagen');
-    const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
-        imgCarta.classList.add('carta');
-
-    return imgCarta;
-}
--- DWCliente/workplace/tema2/practica2/js/usecases/index.js
+++
@@ -1,5 +0,0 @@
-export { crearDeck } from './generate-deck.js';
-export { crearCartaHTML } from './create-carta-html.js';
-export { valorCarta } from './valor-carta.js';
-
-
