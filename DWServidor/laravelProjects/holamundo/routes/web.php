93833ad600acb5cbf65ec95a984921fe1e2b98d1
nombrePrueba <email@email.com>
2025-01-09 18:01:40 +0100

    subiendo laravel

--- DWServidor/laravelProjects/holamundo/routes/web.php
+++ DWServidor/laravelProjects/holamundo/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/holamundo', function () {
+    return "hola mundo page";
+});
+ 
+Route::get('/holamundo/{persona}/{tipo}', function ($persona, $tipo) {
+    return "hola " . $persona . $tipo;
+});
+ 
+
+// esta mal revisar
+Route::get('/holamundo/{persona}/{rol?}', function ($persona, $rol) {
+
+    if($rol != null){
+            return "no tiene rol";
+    }else{
+        return "hola " . $persona . "de rol" .$rol;
+    }
+
+}) -> where('persona', '[a-z]+');
+
+
+
+Route::get('/', [UserController::class, 'indice']);
+
+
+
+
